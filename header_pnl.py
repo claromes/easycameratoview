cc9fb80fd33f3aa4e248f58b3e04dc8376c0df7c
Claromes <claromes@hey.com>
2022-09-03 21:01:30 -0300

    add depress

--- header_pnl.py
+++ header_pnl.py
@@ -13,6 +13,6 @@ class HEADER_MT_Panel(Menu):
       lock = context.space_data.lock_camera
 
       if lock is not False:
-        row.operator(UNLOCK_OT_Camera_View.bl_idname, text='Camera to View', icon='LOCKED')
+        row.operator(UNLOCK_OT_Camera_View.bl_idname, text='Camera to View', icon='LOCKED', depress=True)
       else:
-        row.operator(LOCK_OT_Camera_View.bl_idname, text='Camera to View', icon='UNLOCKED')
+        row.operator(LOCK_OT_Camera_View.bl_idname, text='Camera to View', icon='UNLOCKED', depress=False)
